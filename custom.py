aae919ab1251373fcb7bd9f6c2dbb1614191e71a
Gaetan Guidet <gatgui@gmail.com>
2016-11-16 12:00:26 +0900

    fix osx build issue

--- custom.py
+++ custom.py
@@ -1,27 +1,50 @@
 import os
 import sys
 import platform
+import subprocess
 
 # set these variables
 arnold_dir = ""
+arnold_dir_changed = False
 depn_dir = ""
+depn_dir_changed = False
+
+try:
+   with open("custom.cache", "r") as f:
+      for l in f.readlines():
+         spl = map(lambda x: x.strip(), l.split("="))
+         if len(spl) == 2:
+            if spl[0] == "DEPS_DIR" and os.path.isdir(spl[1]):
+               depn_dir = spl[1]
+            elif spl[0] == "ARNOLD_DIR" and os.path.isdir(spl[1]):
+               arnold_dir = spl[1]
+except:
+   pass
 
 for arg in sys.argv[1:]:
    if arg.startswith("DEPS_DIR="):
       tmp = arg[len("DEPS_DIR="):]
       if os.path.isdir(tmp):
          depn_dir = tmp
+         depn_dir_changed = True
    elif arg.startswith("ARNOLD_DIR="):
       tmp = arg[len("ARNOLD_DIR="):]
       if os.path.isdir(tmp):
          arnold_dir = tmp
+         arnold_dir_changed = True
 
-if platform.system() == "Linux":
-    # Linux specific setup
-    pass
-else:
-    # OSX specific setup (or windows?)
-    pass
+if depn_dir_changed or arnold_dir_changed:
+   with open("custom.cache", "w") as f:
+      f.write("DEPS_DIR=%s\n" % depn_dir)
+      f.write("ARNOLD_DIR=%s\n" % arnold_dir)
+      f.write("\n")
+
+print("Using DEPS_DIR=%s" % depn_dir)
+print("Using ARNOLD_DIR=%s" % arnold_dir)
+
+if platform.system() == "Darwin":
+    # OSX specific setup
+    CXXFLAGS = ["-Wno-unused-local-typedefs"]
 
 # depn dirs
 dep_lib = depn_dir + "/lib"
@@ -78,3 +101,15 @@ WITH_GL = True
 INSTALL_PREFIX = os.path.abspath("dist")
 BUILD_CACHEDIR = os.path.abspath("build")
 
+cwd = os.getcwd()
+gcc_atomic = "%s/include/boost/atomic/detail/gcc-atomic.hpp" % depn_dir
+patch = "%s/gcc-atomic.patch" % cwd
+patch_cmd = "patch -p1 < %s" % patch
+
+os.chdir(os.path.dirname(gcc_atomic))
+p = subprocess.Popen(patch_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+out, _ = p.communicate()
+if p.returncode == 0:
+   print("=== Patching gcc-atomic.hpp ===")
+   print(out)
+os.chdir(cwd)
